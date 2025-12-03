c5d0c039762cf553bd0f6ed5ca8427c6e2dc772c
Chirag Chauhan <chiragvchauhan93@gmail.com>
2025-12-03 17:59:45 +0530

    Create server using http

--- hello-world/index.js
+++ hello-world/index.js
@@ -1,1 +1,8 @@
-console.log("Hello World");
+const http = require("http");
+const PORT = 3000;
+const server = http.createServer((req, res) => {
+  res.end("Don't forgot to subscribe");
+});
+server.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
+});
