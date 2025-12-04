b4bdb13d74c80c793c040c7a2e7e8d9983fc816d
Chirag Chauhan <chiragvchauhan93@gmail.com>
2025-12-04 17:22:27 +0530

    Env changes

--- node-hindi/index.js
+++ node-hindi/index.js
@@ -1,6 +1,7 @@
-// const http = require("http");
+import { config } from "dotenv";
+config();
 import http from "http";
-
+console.log(process.env.TEST);
 http
   .createServer((req, res) => {
     res.end("Welcome to CodewithChirag");
