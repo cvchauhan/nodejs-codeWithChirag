a1cb1bcdd93ea7807bb83e0e63de073a44ed324f
Chirag Chauhan <chiragvchauhan93@gmail.com>
2025-12-04 16:16:43 +0530

    env code

--- hello-world/index.js
+++ hello-world/index.js
@@ -1,5 +1,8 @@
+require("dotenv").config({ path: `.env.${process.env.SERVER}` });
+
 const http = require("http");
-const PORT = 3000;
+const PORT = process.env.PORT;
+console.log(PORT);
 const server = http.createServer((req, res) => {
   res.end("Don't forgot to subscribe");
 });
